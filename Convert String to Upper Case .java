64a0831f9fe2321b57a1580f2257a510b69d1a25
Rambalaji25 <92376545+Rambalaji25@users.noreply.github.com>
2021-11-06 13:12:25 +0530

    Create  Convert String to Upper Case .java

---
+++ Convert String to Upper Case .java
@@ -0,0 +1,20 @@
+ Convert String to Upper Case 
+ 
+ The program must accept a string and and convert the characters in the string to upper case.
+ Then the program must print the modified string as the output. 
+ Example Input/Output 1: 
+ Input: SkillRack
+ Output: SKILLRACK
+ 
+ Example Input/Output 2: 
+ Input: flowers
+ Output: FLOWERS
+ 
+ Java ( 12.0)
+ import java.util.Scanner;
+ public class Hello {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = ; System.out.print();
+    }
+}
