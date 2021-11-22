af73e8f8257a41c68de62f77b27372d4bb76d44c
Rambalaji25 <92376545+Rambalaji25@users.noreply.github.com>
2021-11-22 20:09:22 +0530

    Create rotate digit 180 degree.py

---
+++ rotate digit 180 degree.py
@@ -0,0 +1,51 @@
+The program must accept an integer N as the input. The program must form an integer X by rotating the each digit 180 degrees based on the following conditions.
+- The digits 2 and 5 rotate to each other.
+- The digits 6 and 9 rotate to each other.
+- For the remaining digits (0, 1, 3, 4, 7 and 8), they remain the same.
+Finally, the program must print the sum of N and X as the output.
+
+Boundary Condition(s):
+1 <= N <= 10^8
+
+Input Format:
+The first line contains N.
+
+Output Format:
+The first line contains the sum of N and X.
+
+Example Input/Output 1:
+Input:
+10672
+
+Output:
+21647
+
+Explanation:
+Here N = 10672.
+After rotating each digit by 180 degrees, the integer becomes 10975.
+The sum of 10672 and 10975 is 21647.
+So 21647 is printed as the output.
+
+Example Input/Output 2:
+Input:
+5599
+
+Output:
+7865
+
+#Your code below
+f=input().strip()
+g=''
+for i in f:
+    if i=='2':
+        g+='5'
+    elif i=='5':
+        g+='2'
+    elif i=='9':
+        g+='6'
+    elif i=='6':
+        g+='9'
+    else:
+        g+=i
+print(int(f)+int(g))
+    
